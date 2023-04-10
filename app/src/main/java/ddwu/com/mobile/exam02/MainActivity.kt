42c1b17a492992135eee744f10e71d3cada7b486
nn00rnrn@naver.com <nn00rnrn@naver.com>
2023-04-10 12:45:19 +0900

    Initial commit

---
+++ app/src/main/java/ddwu/com/mobile/exam02/MainActivity.kt
@@ -0,0 +1,25 @@
+package ddwu.com.mobile.exam02
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.widget.Toast
+import ddwu.com.mobile.exam02.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+
+
+}
