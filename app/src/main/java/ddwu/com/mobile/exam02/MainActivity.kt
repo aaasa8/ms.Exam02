e1d3640f0e2515ab31765d3fda4f34ce5c7f4f5e
aaasa8 <113534445+aaasa8@users.noreply.github.com>
2023-04-15 15:55:43 +0900

    Update MainActivity.kt

--- app/src/main/java/ddwu/com/mobile/exam02/MainActivity.kt
+++ app/src/main/java/ddwu/com/mobile/exam02/MainActivity.kt
@@ -39,7 +39,7 @@ class MainActivity : AppCompatActivity() {
 //B) 색을 바꾸는 LongClick 구현해보기
 	inner class MyClick : View.OnLongClickListener{
 		override fun onLongClick(p0: View?): Boolean {
-			binding
+			binding.myCustomView.paintColor = 
 		true
 		}
 	}
